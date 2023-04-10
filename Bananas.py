1b60b7c47a5c12bb3a6b215152be4256a60d7933
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 23:07:57 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -2,26 +2,26 @@ import streamlit as st
 import requests
 from PIL import Image
 from io import BytesIO
-import time
 
 # Define the function to generate the response
 def generate_response(prompt):
     # Insert your code here to generate the response
     # You can use any language model or API you prefer
-    time.sleep(5) # Simulate the response generation process
     response = f"Your prompt: {prompt}\n\nThis is a dummy response."
     return response
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
 
-# Define the sidebar navigation
+# Create the navigation bar
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("", ("Home", "Chat", "Information", "Code Resource", "About Us"))
+selection = st.sidebar.radio("Go to", ["Chat", "Information", "Code Resources", "About Us"])
 
-elif page == "Chat":
-    st.title("Chat with Banana Chatbot")
+# Create the pages
+if selection == "Chat":
+    st.title("Banana Chatbot")
     st.write("Enter a prompt and the chatbot will respond!")
+    
     # Create the text input and Generate button
     prompt = st.text_input("Prompt:")
     if st.button("Generate"):
@@ -29,6 +29,7 @@ elif page == "Chat":
             response = generate_response(prompt)
         st.success("Done!")
         st.write(response)
+        
         # Download the image and display it
         image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
         response = requests.get(image_url)
@@ -37,19 +38,18 @@ elif page == "Chat":
             st.image(img, use_column_width=True)
         except:
             st.warning("Unable to display image.")
-elif page == "Information":
+            
+elif selection == "Information":
     st.title("Information")
-    st.write("This page contains some information about Banana Chatbot.")
-    st.write("Banana Chatbot is a fun chatbot that generates responses to your prompts. It uses advanced machine learning techniques to generate responses that are sometimes humorous, sometimes informative, and sometimes nonsensical. Try it out in the Chat page!")
-    st.write("The chatbot was created by John Doe and Jane Smith, two developers who love bananas and chatbots.")
-elif page == "Code Resource":
-    st.title("Code Resource")
-    st.write("This page contains some resources related to the development of Banana Chatbot.")
-    st.write("The chatbot was developed using Python and Streamlit, an open-source app framework for machine learning and data science.")
-    st.write("If you want to learn more about Streamlit, check out their website at https://streamlit.io/.")
-    st.write("The code for Banana Chatbot is available on GitHub at https://github.com/johndoe/banana-chatbot.")
-elif page == "About Us":
+    st.write("This is the information page. Here you can find information about the chatbot.")
+    st.write("Insert your text here.")
+    
+elif selection == "Code Resources":
+    st.title("Code Resources")
+    st.write("This is the code resources page. Here you can find helpful resources to create your own chatbot.")
+    st.write("Insert your text here.")
+    
+elif selection == "About Us":
     st.title("About Us")
-    st.write("This page contains some information about the creators of Banana Chatbot.")
-    st.write("John Doe is a software developer who loves bananas, Python, and machine learning. He lives in San Francisco, California.")
-    st.write("Jane Smith is a data scientist who loves bananas, statistics, and natural language processing. She lives in New York City, New York.")
+    st.write("This is the about us page. Here you can find information about the developers of the chatbot.")
+    st.write("Insert your text here.")
