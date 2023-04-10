16ac5ea85ce60f3f42cc77ccb7cdfce8cd397d6a
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 23:34:53 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -14,73 +14,35 @@ def generate_response(prompt):
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
-
-# Set the background color and style
-page_bg_img = '''
-<style>
-body {
-background-color: #151515;
-color: white;
-}
-.stButton button {
-background-color: #ffbf00;
-}
-.stTextInput>div>div>input {
-color: white;
-}
-.stTextInput>div>label {
-color: white;
-}
-.stTextInput>div>div>div {
-background-color: #333333;
-}
-.stTextArea>div>div>textarea {
-color: white;
-}
-.stTextArea>div>label {
-color: white;
-}
-.stTextArea>div>div>div {
-background-color: #333333;
-}
-</style>
-'''
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("Banana Chatbot")
 
 # Set up the navigation bar
-navigation = st.sidebar.radio("Navigation", ["Chat", "Information", "Code Resources", "About Us"])
+nav_options = ["Image Generator", "Code Resources", "About Us"]
+nav_selection = st.sidebar.radio("", nav_options)
 
-# Create the chat page
-if navigation == "Chat":
-    st.title("Banana Chatbot")
-    st.write("Enter a prompt and the chatbot will respond!")
+# Set the font and background color for the app
+st.markdown('<style>body { font-family: "Press Start 2P", cursive; background-color: #ffc947;}</style>', unsafe_allow_html=True)
+
+# Show the appropriate page based on the navigation selection
+if nav_selection == "Image Generator":
     # Create the text input and Generate button
-    prompt = st.text_input("Prompt:")
-    if st.button("Generate"):
-        with st.spinner("Generating response..."):
-            response = generate_response(prompt)
-        st.success("Done!")
-        st.write(response)
-        # Download the image and display it
-        image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
-        response = requests.get(image_url)
-        try:
-            img = Image.open(BytesIO(response.content))
-            st.image(img, use_column_width=True)
-        except:
-            st.warning("Unable to display image.")
-
-# Create the information page
-elif navigation == "Information":
-    st.title("Banana Information")
-    st.write("Bananas are a great source of potassium and other nutrients.")
-
-# Create the code resources page
-elif navigation == "Code Resources":
-    st.title("Banana Code Resources")
-    st.write("Here are some helpful resources for coding with bananas.")
-
-# Create the about us page
-else:
-    st.title("About Us")
-    st.write("We are a team of banana enthusiasts who love creating chatbots.")
+    with st.beta_container():
+        st.markdown('<p style="color:red;">Enter a prompt and the image generator will create a response!</p>', unsafe_allow_html=True)
+        prompt = st.text_input("", "")
+        if st.button("Generate"):
+            with st.spinner("Generating response..."):
+                response = generate_response(prompt)
+            st.success("Done!")
+            st.write(response)
+            # Download the image and display it
+            image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
+            response = requests.get(image_url)
+            try:
+                img = Image.open(BytesIO(response.content))
+                st.image(img, use_column_width=True)
+            except:
+                st.warning("Unable to display image.")
+elif nav_selection == "Code Resources":
+    st.write("This is the code resources page.")
+elif nav_selection == "About Us":
+    st.write("This is the about us page.")
