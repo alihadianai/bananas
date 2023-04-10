0b4715cf5c9779bf9f1978a8ca66d79c70a33e76
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 21:38:59 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -4,49 +4,40 @@ from PIL import Image
 from io import BytesIO
 import time
 
-# Translations
-translations = {
-    "en": {
-        "title": "Banana Chatbot",
-        "input_label": "Enter a prompt and the chatbot will respond!",
-        "generate_button": "Generate",
-        "response_message": "Your prompt: {}\n\nThis is a dummy response.",
-        "image_warning": "Unable to display image.",
-        "input_placeholder": "Prompt:"
-    },
-    "ja": {
-        "title": "バナナチャットボット",
-        "input_label": "プロンプトを入力すると、チャットボットが応答します！",
-        "generate_button": "生成する",
-        "response_message": "あなたのプロンプト：{}\n\nこれはダミーの応答です。",
-        "image_warning": "画像を表示できません。",
-        "input_placeholder": "プロンプト："
-    }
-}
-
 # Define the function to generate the response
 def generate_response(prompt):
     # Insert your code here to generate the response
     # You can use any language model or API you prefer
     time.sleep(5) # Simulate the response generation process
-    response = translations[language]["response_message"].format(prompt)
+    response = f"Your prompt: {prompt}\n\nThis is a dummy response."
     return response
 
 # Set up the Streamlit app
-st.set_page_config(page_title=translations[language]["title"], page_icon="🍌", layout="wide")
+st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
 
-language = st.sidebar.selectbox("Select Language / 言語を選択", options=["en", "ja"])
+# Set up the background image
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://i.imgur.com/Lf0vOM4.png");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-st.title(translations[language]["title"])
-st.write(translations[language]["input_label"])
+# Set up the header
+st.write("<h1 style='text-align: center; color: red; font-family: Courier;'>Banana Chatbot</h1>", unsafe_allow_html=True)
 
 # Create the text input and Generate button
-prompt = st.text_input(translations[language]["input_placeholder"])
-if st.button(translations[language]["generate_button"]):
-    with st.spinner(translations[language]["generating_message"]):
+prompt = st.text_input("Prompt:", key="prompt")
+if st.button("Generate"):
+    with st.spinner("Generating response..."):
         response = generate_response(prompt)
-    st.success(translations[language]["done_message"])
-    st.write(response)
+    st.success("Done!")
+    st.write("<p style='color: blue; font-family: American Typewriter;'>Response:</p>", unsafe_allow_html=True)
+    st.write(f"<p style='font-family: American Typewriter;'>{response}</p>", unsafe_allow_html=True)
+
     # Download the image and display it
     image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
     response = requests.get(image_url)
@@ -54,4 +45,4 @@ if st.button(translations[language]["generate_button"]):
         img = Image.open(BytesIO(response.content))
         st.image(img, use_column_width=True)
     except:
-        st.warning(translations[language]["image_warning"])
+        st.warning("Unable to display image.")
