e32c6c3733f8f747a790aa8cc8d3ae8d2036cc09
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 22:56:47 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -15,34 +15,39 @@ def generate_response(prompt):
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon=":banana:", layout="wide")
 
-# Set the background color and font
-page_bg = """
+# Set background color and font
+page_bg = '''
 <style>
 body {
-    background-color: #FF9900;
-    font-family: 'Comic Sans MS', sans-serif;
+background-color: #c0c0c0;
+font-family: Arial, sans-serif;
 }
 </style>
-"""
+'''
 st.markdown(page_bg, unsafe_allow_html=True)
 
-# Add the header
-st.image("https://i.imgur.com/3Mq6g8U.gif", use_column_width=True)
-st.subheader("Welcome to the Banana Chatbot!")
+# Set up the header
+st.write("<h1 style='text-align: center; color: purple;'>Banana Chatbot</h1>", unsafe_allow_html=True)
 
-# Create the text input and Generate button
-prompt = st.text_input("Say something to the chatbot...")
-if st.button("Generate"):
-    with st.spinner("Generating response..."):
-        response = generate_response(prompt)
-    st.success("Done!")
-    st.write(response)
-
-    # Add a retro graphic
-    image_url = "https://i.imgur.com/j9E07Kf.png"
+# Set up the left and right columns
+col1, col2 = st.columns(2)
+with col1:
+    # Create the text input and Generate button
+    prompt = st.text_input("Prompt:", key='prompt')
+    if st.button("Generate", key='generate'):
+        with st.spinner("Generating response..."):
+            response = generate_response(prompt)
+        st.success("Done!")
+        st.write(response)
+with col2:
+    # Display the banana image
+    image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
     response = requests.get(image_url)
     try:
         img = Image.open(BytesIO(response.content))
-        st.image(img, use_column_width=True)
+        st.image(img, width=300)
     except:
         st.warning("Unable to display image.")
+        
+# Set up the footer
+st.write("<p style='text-align: center; color: white;'>Created with love by Bananas Corp © 1995</p>", unsafe_allow_html=True)
