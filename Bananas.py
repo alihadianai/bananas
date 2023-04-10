9b69c7061db8eb4cc2d0961789034da7dc253234
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 22:16:19 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -14,32 +14,30 @@ def generate_response(prompt):
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
-st.set_option('deprecation.showfileUploaderEncoding', False)
 
 # Set up the background image
-bg_image = Image.open("banana_bg.png")
-bg_image = bg_image.resize((700,500))
-page_bg = '''
+page_bg_img = '''
 <style>
 body {
-background-image: url("data:image/png;base64,%s");
+background-image: url("https://i.imgur.com/Lf0vOM4.png");
 background-size: cover;
 }
 </style>
-''' % base64.b64encode(bg_image.getvalue()).decode('utf-8')
-st.markdown(page_bg, unsafe_allow_html=True)
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Set up the header
-st.image("banana_title.png")
-st.subheader("Enter a prompt and the chatbot will respond!")
+st.write("<h1 style='text-align: center; color: red; font-family: Courier;'>Banana Chatbot</h1>", unsafe_allow_html=True)
 
 # Create the text input and Generate button
-prompt = st.text_input("Prompt:")
+prompt = st.text_input("Prompt:", key="prompt")
 if st.button("Generate"):
     with st.spinner("Generating response..."):
         response = generate_response(prompt)
     st.success("Done!")
-    st.write(response)
+    st.write("<p style='color: blue; font-family: American Typewriter;'>Response:</p>", unsafe_allow_html=True)
+    st.write(f"<p style='font-family: American Typewriter;'>{response}</p>", unsafe_allow_html=True)
+
     # Download the image and display it
     image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
     response = requests.get(image_url)
@@ -48,8 +46,3 @@ if st.button("Generate"):
         st.image(img, use_column_width=True)
     except:
         st.warning("Unable to display image.")
-
-# Set up the Information section
-st.subheader("Information")
-st.write("This app is a demonstration of how a chatbot can be used to respond to prompts. In this case, a dummy response is generated after a delay of 5 seconds to simulate the response generation process. The chatbot can be modified to use any language model or API of your choice to generate responses. The app also displays an image in response to the prompt. You can modify the app to display any image of your choice.") 
-st.write("This app was created by [Your Name Here]. © 2023. All rights reserved.")
