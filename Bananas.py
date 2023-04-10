9e534f9032abc69c8713b02a29fa54e1b96baa93
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 23:04:40 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -15,44 +15,28 @@ def generate_response(prompt):
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
 
+# Define the sidebar navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("", ("Home", "Chat", "Information", "Code Resource", "About Us"))
+
 # Define the pages
-pages = {
-    "Home": "This is the home page. Use the navigation bar to access other pages.",
-    "Chat": "Enter a prompt and the chatbot will respond!",
-    "Information": "This page contains information about the chatbot.",
-    "Code Resource": "This page contains the code used to build the chatbot.",
-    "Terms": "This page contains the terms of use for the chatbot.",
-}
-
-# Create the old-school navigation bar
-st.markdown("""
-<style>
-nav {
-    display: flex;
-    background-color: black;
-    padding: 10px 20px;
-}
-nav a {
-    color: white;
-    text-decoration: none;
-    margin-right: 20px;
-    font-size: 20px;
-    font-weight: bold;
-}
-nav a:hover {
-    color: yellow;
-}
-</style>
-""", unsafe_allow_html=True)
-navigation = st.sidebar.radio("Navigate", list(pages.keys()))
-
-# Display the selected page
-if navigation == "Home":
-    st.title("Welcome to Banana Chatbot!")
-    st.write(pages[navigation])
-elif navigation == "Chat":
-    st.title("Banana Chatbot")
-    st.write(pages[navigation])
+if page == "Home":
+    st.title("Welcome to Banana Chatbot")
+    st.write("This is a chatbot that generates responses to your prompts. Try it out in the Chat page!")
+    # Set background color to yellow
+    st.markdown(
+        """
+        <style>
+        body {
+            background-color: #ffff99;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+elif page == "Chat":
+    st.title("Chat with Banana Chatbot")
+    st.write("Enter a prompt and the chatbot will respond!")
     # Create the text input and Generate button
     prompt = st.text_input("Prompt:")
     if st.button("Generate"):
@@ -61,29 +45,26 @@ elif navigation == "Chat":
         st.success("Done!")
         st.write(response)
         # Download the image and display it
-        image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
+        image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
         response = requests.get(image_url)
         try:
             img = Image.open(BytesIO(response.content))
             st.image(img, use_column_width=True)
         except:
             st.warning("Unable to display image.")
-elif navigation == "Information":
+elif page == "Information":
     st.title("Information")
-    st.write(pages[navigation])
-    st.write("This chatbot was created as a demo for educational purposes only.")
-    st.write("The responses are generated using a simple algorithm and are not based on any AI or machine learning model.")
-    st.write("The chatbot is for entertainment purposes only and should not be used as a substitute for professional advice.")
-elif navigation == "Code Resource":
+    st.write("This page contains some information about Banana Chatbot.")
+    st.write("Banana Chatbot is a fun chatbot that generates responses to your prompts. It uses advanced machine learning techniques to generate responses that are sometimes humorous, sometimes informative, and sometimes nonsensical. Try it out in the Chat page!")
+    st.write("The chatbot was created by John Doe and Jane Smith, two developers who love bananas and chatbots.")
+elif page == "Code Resource":
     st.title("Code Resource")
-    st.write(pages[navigation])
-    st.write("Here is the code used to build this chatbot:")
-    with st.beta_expander("View Code"):
-        with open("Bananas.py", "r") as f:
-            st.code(f.read())
-elif navigation == "Terms":
-    st.title("Terms")
-    st.write(pages[navigation])
-    st.write("By using this chatbot, you agree to the following terms of use:")
-    st.write("This chatbot is for entertainment purposes only and should not be used as a substitute for professional advice.")
-    st.write("The creators of this chatbot are
+    st.write("This page contains some resources related to the development of Banana Chatbot.")
+    st.write("The chatbot was developed using Python and Streamlit, an open-source app framework for machine learning and data science.")
+    st.write("If you want to learn more about Streamlit, check out their website at https://streamlit.io/.")
+    st.write("The code for Banana Chatbot is available on GitHub at https://github.com/johndoe/banana-chatbot.")
+elif page == "About Us":
+    st.title("About Us")
+    st.write("This page contains some information about the creators of Banana Chatbot.")
+    st.write("John Doe is a software developer who loves bananas, Python, and machine learning. He lives in San Francisco, California.")
+    st.write("Jane Smith is a data scientist who loves bananas, statistics, and natural language processing. She lives in New York City, New York.")
