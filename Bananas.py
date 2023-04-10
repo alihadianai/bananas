640ce93676480dcd18f59143d41ae2ef176b0801
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 23:10:14 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -2,26 +2,58 @@ import streamlit as st
 import requests
 from PIL import Image
 from io import BytesIO
+import time
 
 # Define the function to generate the response
 def generate_response(prompt):
     # Insert your code here to generate the response
     # You can use any language model or API you prefer
+    time.sleep(5) # Simulate the response generation process
     response = f"Your prompt: {prompt}\n\nThis is a dummy response."
     return response
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
 
-# Create the navigation bar
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", ["Chat", "Information", "Code Resources", "About Us"])
+# Set the background color and style
+page_bg_img = '''
+<style>
+body {
+background-color: #151515;
+color: white;
+}
+.stButton button {
+background-color: #ffbf00;
+}
+.stTextInput>div>div>input {
+color: white;
+}
+.stTextInput>div>label {
+color: white;
+}
+.stTextInput>div>div>div {
+background-color: #333333;
+}
+.stTextArea>div>div>textarea {
+color: white;
+}
+.stTextArea>div>label {
+color: white;
+}
+.stTextArea>div>div>div {
+background-color: #333333;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Create the pages
-if selection == "Chat":
+# Set up the navigation bar
+navigation = st.sidebar.radio("Navigation", ["Chat", "Information", "Code Resources", "About Us"])
+
+# Create the chat page
+if navigation == "Chat":
     st.title("Banana Chatbot")
     st.write("Enter a prompt and the chatbot will respond!")
-    
     # Create the text input and Generate button
     prompt = st.text_input("Prompt:")
     if st.button("Generate"):
@@ -29,27 +61,26 @@ if selection == "Chat":
             response = generate_response(prompt)
         st.success("Done!")
         st.write(response)
-        
         # Download the image and display it
-        image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
+        image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
         response = requests.get(image_url)
         try:
             img = Image.open(BytesIO(response.content))
             st.image(img, use_column_width=True)
         except:
             st.warning("Unable to display image.")
-            
-elif selection == "Information":
-    st.title("Information")
-    st.write("This is the information page. Here you can find information about the chatbot.")
-    st.write("Insert your text here.")
-    
-elif selection == "Code Resources":
-    st.title("Code Resources")
-    st.write("This is the code resources page. Here you can find helpful resources to create your own chatbot.")
-    st.write("Insert your text here.")
-    
-elif selection == "About Us":
+
+# Create the information page
+elif navigation == "Information":
+    st.title("Banana Information")
+    st.write("Bananas are a great source of potassium and other nutrients.")
+
+# Create the code resources page
+elif navigation == "Code Resources":
+    st.title("Banana Code Resources")
+    st.write("Here are some helpful resources for coding with bananas.")
+
+# Create the about us page
+else:
     st.title("About Us")
-    st.write("This is the about us page. Here you can find information about the developers of the chatbot.")
-    st.write("Insert your text here.")
+    st.write("We are a team of banana enthusiasts who love creating chatbots.")
