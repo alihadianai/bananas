56571093329fd26ff3ec1883c69c9224aaa89f03
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 16:36:33 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -3,14 +3,14 @@ from PIL import Image
 import time
 
 # Define the background image
-background_image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
+background_image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"
 
 # Define the function to generate the pixelated banana
 def generate_pixelated_banana(pixel_size):
     # Insert your code here to generate the pixelated banana
     # You can use any image processing library you prefer
     time.sleep(5) # Simulate the image generation process
-    banana_image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
+    banana_image_url = "https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"
     return banana_image_url
 
 # Set up the Streamlit app
@@ -19,7 +19,7 @@ st.markdown(f'<style>body{{background-image: url("{background_image_url}"); back
 
 # Create the parameter input and Generate button
 with st.container():
-    st.image(Image.open("https://imgtr.ee/images/2023/04/10/nQda2.png"), use_column_width=True)
+    st.image(Image.open("https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg"), use_column_width=True)
     pixel_size = st.slider("Pixel size", 1, 100, 10, 1)
     if st.button("Generate"):
         with st.spinner("Generating pixelated banana..."):
