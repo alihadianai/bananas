b1dea942d5d8d8f016d9706cdd71c0e35b08bafb
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 22:59:17 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -14,40 +14,53 @@ def generate_response(prompt):
 
 # Set up the Streamlit app
 st.set_page_config(page_title="Banana Chatbot", page_icon=":banana:", layout="wide")
+background_color = '#F5B7B1'
+text_color = '#212F3D'
+font = 'monospace'
+st.markdown(
+    f"""
+    <style>
+        .reportview-container {{
+            background-color: {background_color};
+            color: {text_color};
+            font-family: {font};
+        }}
+        .stButton button {{
+            background-color: #E74C3C;
+            color: white;
+            border-radius: 8px;
+        }}
+        .stTextInput {{
+            background-color: white;
+            border-radius: 8px;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.title("Banana Chatbot")
+st.subheader("An old-school American AI chatbot")
 
-# Set background color and font
-page_bg = '''
-<style>
-body {
-background-color: #c0c0c0;
-font-family: Arial, sans-serif;
-}
-</style>
-'''
-st.markdown(page_bg, unsafe_allow_html=True)
+# Set up the image for the header
+header_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
+header_response = requests.get(header_url)
+header_img = Image.open(BytesIO(header_response.content))
 
-# Set up the header
-st.write("<h1 style='text-align: center; color: purple;'>Banana Chatbot</h1>", unsafe_allow_html=True)
+# Display the header image
+st.image(header_img, use_column_width=True)
 
-# Set up the left and right columns
-col1, col2 = st.columns(2)
-with col1:
-    # Create the text input and Generate button
-    prompt = st.text_input("Prompt:", key='prompt')
-    if st.button("Generate", key='generate'):
-        with st.spinner("Generating response..."):
-            response = generate_response(prompt)
-        st.success("Done!")
-        st.write(response)
-with col2:
-    # Display the banana image
+# Create the text input and Generate button
+prompt = st.text_input("You say:", "")
+if st.button("Banana say:"):
+    with st.spinner("Thinking..."):
+        response = generate_response(prompt)
+    st.success("Done!")
+    st.write("Banana say:", response)
+    # Download the image and display it
     image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
     response = requests.get(image_url)
     try:
         img = Image.open(BytesIO(response.content))
-        st.image(img, width=300)
+        st.image(img, use_column_width=True)
     except:
         st.warning("Unable to display image.")
-        
-# Set up the footer
-st.write("<p style='text-align: center; color: white;'>Created with love by Bananas Corp © 1995</p>", unsafe_allow_html=True)
