dd5805d8a22539ebee901c9af90901b2d39d96cb
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 18:11:15 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -9,42 +9,55 @@ def generate_response(prompt):
     # Insert your code here to generate the response
     # You can use any language model or API you prefer
     time.sleep(5) # Simulate the response generation process
-    response = f"Your prompt: {prompt}\n\nThis is a dummy response."
+    response = f"あなたのプロンプト：{prompt}\n\nこれはダミーの応答です。"
     return response
 
 # Set up the Streamlit app
-st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
+st.set_page_config(page_title="バナナチャットボット", page_icon="🍌", layout="wide")
 
-# Add custom CSS to set the background image
-st.markdown(
-    """
-    <style>
-    body {
-        background-image: url('https://imgtr.ee/images/2023/04/10/nQda2.png');
-        background-size: cover;
-        background-repeat: no-repeat;
+# Set up Japanese and English translations
+text = {
+    "title": {
+        "jp": "バナナチャットボット",
+        "en": "Banana Chatbot"
+    },
+    "prompt": {
+        "jp": "プロンプトを入力してください：",
+        "en": "Enter a prompt:"
+    },
+    "button": {
+        "jp": "生成する",
+        "en": "Generate"
+    },
+    "response": {
+        "jp": "応答",
+        "en": "Response"
+    },
+    "spinner": {
+        "jp": "応答を生成しています...",
+        "en": "Generating response..."
+    },
+    "image_error": {
+        "jp": "画像を表示できません。",
+        "en": "Unable to display image."
     }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Set the app title and description
-st.title("Banana Chatbot")
-st.write("Enter a prompt and the chatbot will respond!")
+}
 
 # Create the text input and Generate button
-prompt = st.text_input("Prompt:")
-if st.button("Generate"):
-    with st.spinner("Generating response..."):
+st.title(text["title"]["jp"])
+st.write(text["prompt"]["jp"])
+prompt = st.text_input("", key="prompt")
+if st.button(text["button"]["jp"]):
+    with st.spinner(text["spinner"]["jp"]):
         response = generate_response(prompt)
-    st.success("Done!")
+    st.success("完了！")
+    st.write(text["response"]["jp"])
     st.write(response)
     # Download the image and display it
-    image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
+    image_url = "https://imgtr.ee/images/2023/04/10/nQda2.png"
     response = requests.get(image_url)
     try:
         img = Image.open(BytesIO(response.content))
         st.image(img, use_column_width=True)
     except:
-        st.warning("Unable to display image.")
+        st.warning(text["image_error"]["jp"])
