6288dd98e281e4afe2ab4b21bdd66c502e73abe2
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-10 23:47:05 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -13,42 +13,69 @@ def generate_response(prompt):
     return response
 
 # Set up the Streamlit app
-st.set_page_config(page_title="Banana Chatbot", page_icon="🍌", layout="wide")
-st.markdown(
-    f"""
-    <style>
-        .reportview-container {{
-            background: url("https://i.imgur.com/xKpokZu.png")
-        }}
-        .sidebar .sidebar-content {{
-            background: url("https://i.imgur.com/kk4lzL9.png")
-        }}
-        .btn-outline-secondary {{
-            color: #FFFFFF;
-        }}
-        .stTextInput > div > input {{
-            color: red;
-            font-family: 'Comic Sans MS', cursive, sans-serif;
-        }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.set_page_config(page_title="Banana Image Generator", page_icon="🍌", layout="wide")
 st.title("Banana Image Generator")
-st.write("Enter a prompt and the image generator will respond!")
+st.write("Enter a prompt and the image generator will create an image!")
+
+# Set the background color and font
+page_bg_color = '''
+<style>
+body {
+background-color: #e6e6e6;
+font-family: "Brush Script MT", cursive;
+}
+</style>
+'''
+st.markdown(page_bg_color, unsafe_allow_html=True)
 
 # Create the text input and Generate button
-prompt = st.text_input("Prompt:")
+prompt = st.text_input("Prompt:", key="prompt_input")
+prompt_style = "<style> .css-1yqbge4 { color: red; font-size: 24px; }</style>"
+st.markdown(prompt_style, unsafe_allow_html=True)
+
 if st.button("Generate"):
     with st.spinner("Generating image..."):
-        response = generate_response(prompt)
-    st.success("Done!")
-    st.write(response)
-    # Download the image and display it
-    image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
-    response = requests.get(image_url)
-    try:
-        img = Image.open(BytesIO(response.content))
-        st.image(img, use_column_width=True)
-    except:
-        st.warning("Unable to display image.")
+        # Insert your code here to generate the image
+        # You can use any image generation model or API you prefer
+        image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
+        response = requests.get(image_url)
+        try:
+            img = Image.open(BytesIO(response.content))
+            st.image(img, use_column_width=True)
+        except:
+            st.warning("Unable to display image.")
+
+# Create the navigation bar
+menu = ["Image Generator", "Information", "Code Resources", "About Us"]
+choice = st.sidebar.selectbox("Select a page", menu)
+
+# Display the selected page
+if choice == "Image Generator":
+    st.write("Enter a prompt and the image generator will create an image!")
+    prompt = st.text_input("Prompt:", key="prompt_input")
+    prompt_style = "<style> .css-1yqbge4 { color: red; font-size: 24px; }</style>"
+    st.markdown(prompt_style, unsafe_allow_html=True)
+
+    if st.button("Generate"):
+        with st.spinner("Generating image..."):
+            # Insert your code here to generate the image
+            # You can use any image generation model or API you prefer
+            image_url = "https://thumb.ac-illust.com/a8/a8ccf142b92269fcccc3e8f92b5bba0e_t.jpeg"
+            response = requests.get(image_url)
+            try:
+                img = Image.open(BytesIO(response.content))
+                st.image(img, use_column_width=True)
+            except:
+                st.warning("Unable to display image.")
+                
+elif choice == "Information":
+    st.write("This is the information page.")
+    st.write("Here you can find more information about our image generator.")
+    
+elif choice == "Code Resources":
+    st.write("This is the code resources page.")
+    st.write("Here you can find resources and examples of code for creating your own image generator.")
+    
+elif choice == "About Us":
+    st.write("This is the about us page.")
+    st.write("Here you can learn more about our team and the development of this image generator.")
