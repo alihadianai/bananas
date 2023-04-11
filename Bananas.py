b112b8ca2cc877672ee3ccb4f71c91381459660d
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-11 20:28:48 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -1,5 +1,5 @@
-pip install Flask
-pip install SQLAlchemy
+install Flask
+install SQLAlchemy
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
