6604ca3b66c9bbf73790658d2616a4d13d549490
alihadianai <129296626+alihadianai@users.noreply.github.com>
2023-04-12 10:36:37 +0330

    Update Bananas.py

--- Bananas.py
+++ Bananas.py
@@ -9,6 +9,7 @@ class ProjectManagementApp:
             'Projects': self.render_projects_page,
         }
         self.current_user = None
+        self.current_page = None
         self.tasks_df = pd.DataFrame(columns=['Task name', 'Task description', 'Due date'])
         self.projects_df = pd.DataFrame(columns=['Project name', 'Project description', 'Status'])
         
@@ -57,56 +58,31 @@ class ProjectManagementApp:
             except:
                 st.error('Error creating project')
     
+    def render_login_page(self):
+        st.title('Welcome to the project management app!')
+        username = st.text_input('Username')
+        password = st.text_input('Password', type='password')
+        if st.button('Log in'):
+            if self.authenticate(username, password):
+                st.success('Logged in as {}'.format(username))
+                self.current_page = 'Home'
+            else:
+                st.error('Incorrect username or password')
+
     def run(self):
         st.sidebar.title('Navigation')
-        current_page_name = st.sidebar.selectbox('Select a page', list(self.pages.keys()))
-        current_page = self.pages[current_page_name
-    if self.current_user is None:
-        self.render_login_page()
+        if self.current_page is None:
+            self.render_login_page()
+        elif self.current_page == 'Home':
+            st.sidebar.selectbox('Select a page', list(self.pages.keys()), index=0)
+            self.render_home_page()
+        elif self.current_page == 'Tasks':
+            st.sidebar.selectbox('Select a page', list(self.pages.keys()), index=1)
+            self.render_tasks_page()
+        elif self.current_page == 'Projects':
+            st.sidebar.selectbox('Select a page', list(self.pages.keys()), index=2)
+            self.render_projects_page()
 
-    elif self.current_page == 'Home':
-        st.title('Welcome to the home page, {}'.format(self.current_user))
-
-    elif self.current_page == 'Tasks':
-        self.render_tasks_page()
-
-    elif self.current_page == 'Projects':
-        self.render_projects_page()
-
-def render_login_page(self):
-    st.title('Welcome to the project management app!')
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
-    if st.button('Log in'):
-        if self.authenticate(username, password):
-            st.success('Logged in as {}'.format(username))
-            self.current_page = self.pages['Home']
-        else:
-            st.error('Incorrect username or password')
-
-def render_tasks_page(self):
-    st.title('Task management')
-    task_name = st.text_input('Task name')
-    task_description = st.text_area('Task description')
-    task_due_date = st.date_input('Due date')
-    if st.button('Add task'):
-        try:
-            self.tasks_df.loc[len(self.tasks_df)] = [task_name, task_description, task_due_date]
-            st.write('Task added:')
-            st.write(self.tasks_df)
-        except:
-            st.error('Error adding task')
-
-def render_projects_page(self):
-    st.title('Project tracking')
-    project_name = st.text_input('Project name')
-    project_description = st.text_area('Project description')
-    project_status = st.selectbox('Status', ['Not started', 'In progress', 'Completed'])
-    if st.button('Create project'):
-        try:
-            self.projects_df.loc[len(self.projects_df)] = [project_name, project_description, project_status]
-            st.write('Project created:')
-            st.write(self.projects_df)
-        except:
-            st.error('Error creating project')
-
+if __name__ == '__main__':
+    app = ProjectManagementApp()
+    app.run()
